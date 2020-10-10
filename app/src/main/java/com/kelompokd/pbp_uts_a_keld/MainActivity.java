34e6f49e4920ee708210b9039082f3f871167c11
yobelyuantomo <yobelyuantomo@gmail.com>
2020-10-10 11:18:16 +0800

    PBP_UTS_A_KelD

---
+++ app/src/main/java/com/kelompokd/pbp_uts_a_keld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kelompokd.pbp_uts_a_keld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
