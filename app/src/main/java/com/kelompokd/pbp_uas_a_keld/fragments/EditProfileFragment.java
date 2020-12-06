c28011f5e59d252e1237bd89074ee1ec692665a4
Asus <juliyapradnyawati@gmail.com>
2020-12-06 10:43:02 +0800

    file

--- app/src/main/java/com/kelompokd/pbp_uas_a_keld/fragments/EditProfileFragment.java
+++ app/src/main/java/com/kelompokd/pbp_uas_a_keld/fragments/EditProfileFragment.java
@@ -1,5 +1,6 @@
 package com.kelompokd.pbp_uas_a_keld.fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,13 +10,38 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.kelompokd.pbp_uas_a_keld.API.MorentAPI;
 import com.kelompokd.pbp_uas_a_keld.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.android.volley.Request.Method.POST;
 
 public class EditProfileFragment extends Fragment {
 
+    private TextInputEditText nama, email, alamat;
+    //TextInputLayout layout_noKartu, layout_payemntMethod;
     MaterialButton btnBatal, btnSimpan;
+    View view;
+    //AutoCompleteTextView exposedDropdownPayment;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,5 +65,79 @@ public class EditProfileFragment extends Fragment {
         });
 
         return root;
+    }
+
+    public void editProfile(String , String img_pesanan, String durasi_pesan,
+                                String tanggal_pesanan, String tanggal_transaksi, String nama, String email, String alamat,
+                                String jenis_pembayaran, String no_kartu, int selisih){
+
+        //Tambahkan tambah buku disini
+        //Pendeklarasian queue
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        final ProgressDialog progressDialog;
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("loading....");
+        progressDialog.setTitle("Mengedit data transaksi");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+
+        //Memulai membuat permintaan request menghapus data ke jaringan
+        StringRequest stringRequest = new StringRequest(POST, MorentAPI.URL_UPDATE_PESANAN + pesanan.getIdPesanan(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Disini bagian jika response jaringan berhasil tidak terdapat ganguan/error
+                progressDialog.dismiss();
+                try {
+                    //Mengubah response string menjadi object
+                    JSONObject obj = new JSONObject(response);
+                    //obj.getString("message") digunakan untuk mengambil pesan status dari response
+
+                    if(obj.getString("message").equals("Update Pesanan Success"))
+                    {
+                        Navigation.findNavController(view).navigate(R.id.action_nav_edit_pesanan_wisata_to_nav_lihatPesanan);
+                    }
+                    //obj.getString("message") digunakan untuk mengambil pesan message dari response
+                    Toast.makeText(getContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Disini bagian jika response jaringan terdapat ganguan/error
+                progressDialog.dismiss();
+                NetworkResponse networkResponse = error.networkResponse;
+
+                if (networkResponse != null && networkResponse.data != null) {
+                    String jsonError = new String(networkResponse.data);
+                    Toast.makeText(getContext(), jsonError, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams()
+            {
+                /*
+                    Disini adalah proses memasukan/mengirimkan parameter key dengan data value,
+                    dan nama key nya harus sesuai dengan parameter key yang diminta oleh jaringan
+                    API.
+                */
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("nama", nama);
+                params.put("tglLahir", tglLahir);
+                params.put("jenisKelamin", jenisKelamin);
+                params.put("email", email);
+                params.put("alamat", alamat);
+
+
+                return params;
+            }
+        };
+
+        //Disini proses penambahan request yang sudah kita buat ke reuest queue yang sudah dideklarasi
+        queue.add(stringRequest);
     }
 }
